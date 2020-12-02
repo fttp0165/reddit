85a264ed05ff65a27d56a8e73a33b8aad9239917
benny <benny08118@gmail.com>
2020-12-03 01:07:11 +0800

    modify radits to redits

--- test/controllers/reddits_controller_test.rb
+++ test/controllers/reddits_controller_test.rb
@@ -1,6 +1,6 @@
 require 'test_helper'
 
-class RadditsControllerTest < ActionDispatch::IntegrationTest
+class RedditsControllerTest < ActionDispatch::IntegrationTest
   # test "the truth" do
   #   assert true
   # end
