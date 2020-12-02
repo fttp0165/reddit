55ed7aca59505a3011f9a6ead51de541fb25d513
benny <benny08118@gmail.com>
2020-12-03 01:00:42 +0800

    create new project Raddit add devise and acts_as_votable finish:draft sing_in

---
+++ app/controllers/admin_controller.rb
@@ -0,0 +1,16 @@
+class AdminController < ApplicationController
+    def create_session
+		
+		user=FlowerUser.find_by(email: params[:email],password: params[:password])
+		if user
+		session[:current_user_id]=user.id
+		flash[:notice]="登入成功"
+		redirect_to action: :index ,controller: :flower_products
+		return
+		end
+		flash[:notice]="登入失敗"
+		redirect_to action: :log_in
+		#redirect_to action: :new
+    end
+ 
+end
---
+++ app/helpers/raddits_helper.rb
@@ -0,0 +1,2 @@
+module RadditsHelper
+end
