dad2f5ad257117b1059e9f6b6e3aa6defd052d3f
benny <benny08118@gmail.com>
2020-12-04 01:08:31 +0800

    completed draft navbar which and add 'login' and 'sign up' their respective button in navbar.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,15 +1,5 @@
 class ApplicationController < ActionController::Base
-    def create_session
-		
-		user=FlowerUser.find_by(email: params[:email],password: params[:password])
-		if user
-		session[:current_user_id]=user.id
-		flash[:notice]="登入成功"
-		redirect_to action: :index ,controller: :flower_products
-		return
-		end
-		flash[:notice]="登入失敗"
-		redirect_to action: :log_in
-		#redirect_to action: :new
-	end
+  require File.expand_path('../boot', __FILE__)
+  require 'rails/all'
+  require 'devise'
 end
